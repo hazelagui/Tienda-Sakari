8d49946cddb64946d50c41ec48347151390444bb
hazelagui <hazelaguilarquiros@gmail.com>
2025-11-18 22:26:56 -0600

    PARTE 1.1

---
+++ Sakari/src/main/java/Sakari/SakariApplication.java
@@ -0,0 +1,13 @@
+package Sakari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakariApplication.class, args);
+	}
+
+}
---
+++ Sakari/src/test/java/Sakari/SakariApplicationTests.java
@@ -0,0 +1,13 @@
+package Sakari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
